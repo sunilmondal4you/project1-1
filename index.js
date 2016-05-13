56d8e13cad91023c9d274b647e38a52158a75efe
viveking <vivekk252@gmail.com>
2016-05-13 12:47:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,151 @@
+/**
+ * Created by abc on 13/05/2016.
+ */
+
+var app = angular.module('myapp', []);
+
+app.controller('myctrl', function ($scope) {
+
+
+    $scope.image = [{
+        "source": 'image/13090542_1125303050867545_1829714887_n.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/13101066_1125303044200879_1029588968_n (1).jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/13115673_1125303070867543_1331181805_n.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/13141084_1125303067534210_976129631_n.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/20160205_180814.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/20160205_180835.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0003.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0008.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160408-WA0002.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0002.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0010.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0011.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0013.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0014.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0019.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0024.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0026.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0031.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0033.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0034.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0035.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0040.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }, {
+        "source": 'image/IMG-20160410-WA0036.jpg'
+        , "likecount": 0
+        , "dislikecount": 0
+        , "comment": []
+        , "newcomment": []
+    }];
+
+
+});
